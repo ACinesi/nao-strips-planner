150fe722d690d176d2c290f54aeb012acf242b07
Andrea Cinesi <andrea.cinesi@hotmail.it>
2017-11-01 20:30:07 +0100

    added Nao class in sandbox

--- Robot Commands/sandbox.py
+++ Robot Commands/sandbox.py
@@ -1,6 +1,35 @@
-import naoqi as qi
 import argparse
+import pprint
+import time
+
 import qi
+
+
+class Nao(object):
+    def __init__(self, session):
+        self.session = session
+
+    def get_posture(self):
+        posture_service = session.service("ALRobotPosture")
+        posture = posture_service.getPosture()
+        return posture
+
+    def go_to_posture(self, posture_name):
+        posture_service = session.service("ALRobotPosture")
+        result = posture_service.goToPosture(posture_name, 1.0)
+        return result
+
+    def hand_full(self):
+        motion_service = session.service("ALMotion")
+        motion_service.setAngles("LHand", 0.0, 0.25)
+        time.sleep(2.0)
+        hand_angle = motion_service.getAngles("LHand", True)
+        if hand_angle < 0.25:
+            result = False
+        else:
+            result = True
+        return result
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -13,4 +42,9 @@ parser.add_argument("--port", type=int, default=9559, help="Naoqi port number")
 args = parser.parse_args()
 session = qi.Session()
 session.connect("tcp://" + args.ip + ":" + str(args.port))
+
 #tts_proxy = session.service("ALTextToSpeech")
+
+nao = Nao(session)
+
+print nao.hand_full()
